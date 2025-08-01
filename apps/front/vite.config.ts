e0982d9f6064d65dec7aa8e2740ca8aa51289744
Emil0363000 <mimi03600@gmail.com>
2025-08-01 14:43:10 +0200

    installation css

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
      // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
      tanstackRouter({
       target: 'react',
+
       autoCodeSplitting: true,
+    
     }),
     react(),
+    tailwindcss(),
   ],
   server: {
     watch: {
