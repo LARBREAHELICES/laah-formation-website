b53a49567ea96a9fbabe0824a5d59f42472676ce
Antoine07 <antoine.lucsko@wanadoo.fr>
2025-08-13 14:57:07 +0200

    modification formation service et endpoints

--- apps/api/app/endpoints/formation.py
+++ apps/api/app/endpoints/formation.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
-
 from typing import  List
 from app.database import get_db
 from app.services.FormationService import FormationService
-from app.schemas.schema_app import FormationRead
+from app.schemas.schema_app import FormationRead, FormationCreate, FormationUpdate
 
 from sqlmodel import Session
 
@@ -35,3 +34,27 @@ def get(
         return JSONResponse(status_code=404, content={"message": "No formation found"})
     
     return formation
+
+
+# --- CREATE ---
+@router.post("/formation", response_model=FormationRead, status_code=201)
+def create(data: FormationCreate, session: Session = Depends(get_db)):
+    formation = FormationService(session).create(data)
+    
+    return formation
+
+# --- UPDATE ---
+@router.put("/formation/{id}", response_model=FormationRead)
+def update(id: str, data: FormationUpdate, session: Session = Depends(get_db)):
+    formation = FormationService(session).update(id, data)
+    if not formation:
+        return JSONResponse(status_code=404, content={"message": "Formation not found"})
+    return formation
+
+# --- DELETE ---
+@router.delete("/formation/{id}", status_code=204)
+def delete(id: str, session: Session = Depends(get_db)):
+    deleted = FormationService(session).delete(id)
+    if not deleted:
+        return JSONResponse(status_code=404, content={"message": "Formation not found"})
+    return JSONResponse(status_code=204, content={"message": "Formation deleted"})
